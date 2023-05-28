17150ff71272deb0c995c0af1d86344012696f26
vadim-moshev <reg-vadim-moshev@yandex.ru>
2023-05-28 14:29:31 +0500

    move app styles to separated file

--- src/App.vue
+++ src/App.vue
@@ -5,47 +5,7 @@
   </main>
   <footer class="footer"></footer>
 
-
   <!-- <router-view /> -->
 </template>
 
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  min-height: 100%;
-}
-
-.header {
-  height: 50px;
-  background-color: wheat;
-}
-
-.main {
-  height: 100%;
-  flex: auto;
-}
-
-.content-wapper {
-  height: 100px;
-  background-color: navy;
-  width: 616px;
-  margin: 0 auto;
-}
-
-.footer {
-  height: 50px;
-  background-color: aqua;
-}
-</style>
+<style src="./App.css"></style>
