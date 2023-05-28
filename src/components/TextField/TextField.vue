c0570a2bb034052e59d73ec54046c8ddf4b71c01
vadim-moshev <reg-vadim-moshev@yandex.ru>
2023-05-28 21:07:25 +0500

    create text input component

---
+++ src/components/TextField/TextField.vue
@@ -0,0 +1,14 @@
+<template>
+  <label class="text-field__wrapper">
+    <div class="text-field__caption">Подпись</div>
+    <input class="text-field__input" type="text" />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "TextField",
+};
+</script>
+
+<style scoped src="./TextField.css"></style>
--- src/views/FormView/FormView.vue
+++ src/views/FormView/FormView.vue
@@ -1,16 +1,19 @@
 <template>
   <form class="form-view">
     <ViewHeader class="form-view__header">Персональные данные</ViewHeader>
+    <TextField />
   </form>
 </template>
 
 <script>
 import ViewHeader from "@/components/ViewHeader/ViewHeader";
+import TextField from "@/components/TextField/TextField";
 
 export default {
   name: "FormView",
   components: {
     ViewHeader,
+    TextField,
   },
 };
 </script>
