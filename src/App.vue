3256891607afdee7920476ad356e9273f6a9e99c
vadim-moshev <reg-vadim-moshev@yandex.ru>
2023-05-28 13:40:37 +0500

    add sublime project file

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -13,6 +13,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
 }
 
 nav {
