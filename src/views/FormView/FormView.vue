e3531c17f4d4c1f706b3bde568b70c75f6c370b5
vadim-moshev <reg-vadim-moshev@yandex.ru>
2023-05-28 19:42:06 +0500

    add empty form in FormView

--- src/views/FormView/FormView.vue
+++ src/views/FormView/FormView.vue
@@ -1,4 +1,6 @@
-<template>формочка!</template>
+<template>
+  <form class="form-view"></form>
+</template>
 
 <script>
 export default {
@@ -6,4 +8,4 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped src="./FormView.css"></style>
