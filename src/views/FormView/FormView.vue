a28624ec9ee3cccfb2871225d367eaf00f4c103c
vadim-moshev <reg-vadim-moshev@yandex.ru>
2023-05-28 21:57:17 +0500

    group form elemnts into sections

--- src/views/FormView/FormView.vue
+++ src/views/FormView/FormView.vue
@@ -1,7 +1,16 @@
 <template>
   <form class="form-view">
-    <ViewHeader class="form-view__header">Персональные данные</ViewHeader>
-    <TextField caption="Имя" />
+    <section class="form-view_section">
+      <ViewHeader class="form-view__header">Персональные данные</ViewHeader>
+      <TextField caption="Имя" />
+      <TextField caption="Возраст" />
+    </section>
+    <section class="form-view_section">
+      <div class="children-control-panel">
+        <ViewHeader class="form-view__header">Дети (макс. 5)</ViewHeader>
+        <div>кнопк0</div>
+      </div>
+    </section>
   </form>
 </template>
 
