b00ea54c2c5430f9f435ceb390e35f76153f106e
vadim-moshev <reg-vadim-moshev@yandex.ru>
2023-05-29 19:08:34 +0500

    add append child functionality

--- src/views/FormView/FormView.vue
+++ src/views/FormView/FormView.vue
@@ -8,13 +8,18 @@
     <section class="form-view_section">
       <div class="children-control-panel">
         <ViewHeader>Дети (макс. 5)</ViewHeader>
-        <button type="button" class="form-view__add-child-button">
+        <button
+          type="button"
+          class="form-view__add-child-button"
+          v-if="!childrenCountLimitReached"
+          @click="addChild"
+        >
           <img :src="cross" alt="" />
           <span>Добавить ребёнка</span>
         </button>
       </div>
-      <div class="child-blocks-container">
-        <ChildBlock />
+      <div v-if="children.length > 0" class="child-blocks-container">
+        <ChildBlock v-for="(child, idx) in children" :key="idx" />
       </div>
     </section>
     <button type="button" class="form-view__save-button">Сохранить</button>
@@ -26,6 +31,8 @@ import ViewHeader from "@/components/ViewHeader/ViewHeader";
 import TextField from "@/components/TextField/TextField";
 import ChildBlock from "@/components/ChildBlock/ChildBlock";
 
+const MAX_CHILDREN_COUNT = 5;
+
 export default {
   name: "FormView",
   components: {
@@ -33,9 +40,29 @@ export default {
     TextField,
     ChildBlock,
   },
+  data() {
+    return {
+      children: [],
+      childrenCount: 0,
+    };
+  },
   computed: {
     cross() {
       return require("@/assets/cross.svg");
+    },
+    childrenCountLimitReached() {
+      return this.childrenCount === MAX_CHILDREN_COUNT;
+    },
+  },
+  methods: {
+    addChild() {
+      if (this.childrenCount === MAX_CHILDREN_COUNT) {
+        return;
+      }
+
+      const newChild = { name: "", age: 0 };
+      this.children.push(newChild);
+      this.childrenCount++;
     },
   },
 };
